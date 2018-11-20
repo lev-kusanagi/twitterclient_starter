d36936c5d5b21475309c0636dfcbbaf18ddc5831
Lev Kusanagi <levkusanagi@gmail.com>
2018-11-20 05:48:52 +0100

    Create bridge header to import Import STTwitter

---
+++ twitterclient_start/test.m
@@ -0,0 +1,9 @@
+//
+//  test.m
+//  twitterclient_start
+//
+//  Created by Javier Mares Romero on 11/20/18.
+//  Copyright © 2018 letsbuildthatapp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
